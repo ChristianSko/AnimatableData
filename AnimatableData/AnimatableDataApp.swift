d261549c739e7132ecacfdd6a6460c2c9c73aeeb
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-04 13:20:59 +0200

    Initial Commit

---
+++ AnimatableData/AnimatableDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatableDataApp.swift
+//  AnimatableData
+//
+//  Created by Skorobogatow, Christian on 4/8/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimatableDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatableData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatableData
+//
+//  Created by Skorobogatow, Christian on 4/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
